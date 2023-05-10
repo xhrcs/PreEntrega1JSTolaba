3916f9c2b4d40d03c77d00f5ee329ae855fc4fc9
xhrcs1 <xhrcs1@gmail.com>
2023-05-10 20:53:27 -0300

    ddadad

---
+++ app.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Ingrese su nombre")
+
+function Bienvenida(){
+    alert ("Bienvenido" + " " + nombre + " " + 'a nuestra pizzera, a continuacion te mostraremos nuestras opciones para que eligas la que mas te guste 1:pizza comun  2:pizza especial  3:pizza napolitana')
+}
+
+Bienvenida()
