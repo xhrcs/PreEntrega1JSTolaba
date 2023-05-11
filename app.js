4228ed7ea7290f9839121c1894be05b0c8149724
xhrcs1 <xhrcs1@gmail.com>
2023-05-10 22:27:48 -0300

    js simulador itneractivo

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
 let nombre = prompt("Ingrese su nombre")
 
 function Bienvenida(){
-    alert ("Bienvenido" + " " + nombre + " " + 'a nuestra pizzera, a continuacion te mostraremos nuestras opciones para que eligas la que mas te guste 1:pizza comun  2:pizza especial  3:pizza napolitana')
+    pizzaElegida = prompt("Bienvenido" + " " + nombre + " " + 'a nuestra pizzera, a continuacion te mostraremos nuestras opciones para que eligas la que mas te guste \n 1:pizza comun \n 2:pizza especial \n 3:pizza napolitana')
+    if(pizzaElegida ==="1"){
+        alert('Usted a seleccionado pizza comun con un valor de'  + ' ' + precioComun)
+    }
+    else if(pizzaElegida==="2"){
+        alert('Usted a seleccionado pizza especial con un valor de' + ' ' + precioEspecial)
+    }
+    else if(pizzaElegida==="3"){
+        alert('Usted a seleccionado pizza napolitana con un valor de'  + ' ' + precioNapolitana)
+    }
+    else if(pizzaElegida!="1. 2. 3"){
+        valorInvalido()
+    }
 }
 
+function valorInvalido(){
+    opciones = prompt('El valor ingresada no es valida, por favor eliga una de las siguientes opciones \n 1: Volver a comprar \n 2: Finalizar')
+
+}
+
+function MuchasGracias(){
+    alert('Muchas gracias por elegirnos, lo esperamos pronto!')
+}
+
+
+pizzaElegida =
+precioComun = 1000
+precioEspecial = 1200
+precioNapolitana = 1500
+
+
 Bienvenida()
+
+while(opciones!=""){
+    if (opciones==="1") {
+        Bienvenida()
+    }
+    else if(opciones==="2"){
+        MuchasGracias()
+    }
+}
